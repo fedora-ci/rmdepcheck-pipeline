410273a198400c12d61d4c72e5fa2876d6249bd8
Cristian Le <git@lecris.dev>
2025-12-04 10:35:28 +0100

    Use logger more consistently

--- run_rmdepcheck.py
+++ run_rmdepcheck.py
@@ -41,7 +41,7 @@ def get_distro_build(distro_name: str) -> str:
 
 def main(args: argparse.Namespace) -> None:
     repo_path: Path = args.workdir / "repo"
-    out = subprocess.run(
+    subprocess.run(
         [
             "rmdepcheck.py",
             KOJI_BASE.format(
@@ -50,14 +50,9 @@ def main(args: argparse.Namespace) -> None:
             ),
             f"file://{repo_path}",
         ],
-        check=False,
+        check=True,
     )
-    if out.returncode == 0:
-        print("All is good!")
-    else:
-        print("Rmdepcheck failed!")
-        # exit code must be 1 to mark as test failure and not general error
-        raise SystemExit(out.returncode)
+    logger.info("All is good!")
 
 
 if __name__ == "__main__":
@@ -72,4 +67,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args)
+    try:
+        main(args)
+    except subprocess.CalledProcessError as exc:
+        logger.error("Rmdepcheck failed!", exc_info=exc)
+        raise SystemExit(1)
