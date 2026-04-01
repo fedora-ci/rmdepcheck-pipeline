fb1b4fc1de942d5242683a148a16d525ac2d3649
Cristian Le <git@lecris.dev>
2026-04-01 13:18:35 +0200

    Simplify the processing

--- run_rmdepcheck.py
+++ run_rmdepcheck.py
@@ -11,47 +11,39 @@ import os
 import subprocess
 from pathlib import Path
 
-from fedora_distro_aliases import (
-    filter_distro,
-    Distro,
-    bodhi_active_releases,
-    get_distro_aliases,
-)
-
-aliases = get_distro_aliases()
-# Workaround the fact that eln is not in the aliases right now
-# https://github.com/rpm-software-management/fedora-distro-aliases/issues/30
-eln_distro = [
-    Distro.from_bodhi_release(release)
-    for release in bodhi_active_releases()
-    if release["branch"] == "eln"
-]
-aliases["eln"] = eln_distro
+from fedora_distro_aliases import bodhi_active_releases
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(Path(__file__).name)
 
-# TODO: Move this into Fedora-CI tmt profile
 KOJI_BASE = r"https://kojipkgs.fedoraproject.org/repos/{distro_build}/latest/{arch}"
+"""
+Koji build base repo used for the rmdepcheck base repo.
+"""
+FEDORA_ID_PREFIXES = {
+    "FEDORA",
+    "FEDORA-EPEL",
+    "FEDORA-EPEL-NEXT",  # Currently only used by epel9-next
+    # Skipping FEDORA-CONTAINER and FEDORA-FLATPAK
+}
+"""
+Supported ``ID Prefix`` of the releases in https://bodhi.fedoraproject.org/releases.
+"""
+
 
 def get_distro_build(dist_git_branch: str) -> str:
-    distro_info = filter_distro(aliases, branch=dist_git_branch)
-    if not distro_info:
-        logger.error(f"Could not identify distro for branch '{dist_git_branch}'")
+    releases = [
+        release
+        for release in bodhi_active_releases()
+        if release["id_prefix"] in FEDORA_ID_PREFIXES
+        and release["branch"] == dist_git_branch
+    ]
+
+    if not releases or len(releases) > 1:
+        logger.error(f"Could not identify release for branch '{dist_git_branch}'")
         exit(1)
-    # Seems we have many special cases for each branch :/
-    match distro_info.product:
-        case "fedora":
-            if distro_info.branch == "eln":
-                return "eln-build"
-            else:
-                return f"f{distro_info.version_number}-build"
-        case "epel":
-            # Note this does not account for epel9-next
-            return f"epel{distro_info.version_number}-build"
-        case _:
-            logger.error(f"Unrecognized distro.product '{distro_info.product}' of '{dist_git_branch}'")
-            exit(1)
+    # Seems like we can use the dist_tag to construct the koji build tag for all cases
+    return f"{releases[0]['dist_tag']}-build"
 
 
 def main(args: argparse.Namespace) -> None:
@@ -59,10 +51,12 @@ def main(args: argparse.Namespace) -> None:
     subprocess.run(
         [
             "rmdepcheck.py",
+            # Base repo
             KOJI_BASE.format(
                 distro_build=get_distro_build(args.dist_git_branch),
                 arch=args.arch,
             ),
+            # Repo to be checked
             f"file://{repo_path}",
         ],
         check=True,
